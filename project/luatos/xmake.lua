a4f35bb23c5855ebcb84286e721b4508fc58e341
Wendal Chen <wendal1985@gmail.com>
2024-06-05 14:44:16 +0800

    fix: fdb库没有加入到luatos的编译里

--- project/luatos/xmake.lua
+++ project/luatos/xmake.lua
@@ -125,6 +125,9 @@ target(project_name,function()
     -- fskv
     add_includedirs(luatos_root.."/components/fskv",{public = true})
     add_files(luatos_root.."/components/fskv/*.c")
+    -- fdb
+    add_includedirs(luatos_root.."/components/flashdb/inc",{public = true})
+    add_files(luatos_root.."/components/flashdb/src/*.c")
     -- fonts
     add_includedirs(luatos_root.."/components/luatfonts",{public = true})
     add_files(luatos_root.."/components/luatfonts/*.c")
