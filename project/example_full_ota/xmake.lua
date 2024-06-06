8a507b80cb08f9052098beca84e882966adc12aa
alienwalker <alienwalker@sina.com>
2024-06-06 09:12:35 +0800

    fix:AIR780EQ全量升级失败

--- project/example_full_ota/xmake.lua
+++ project/example_full_ota/xmake.lua
@@ -13,7 +13,7 @@ target(project_name,function()
 
     local chip_target = nil
     if has_config("chip_target") then chip_target = get_config("chip_target") end
-    -- 程序区缩小到1b7000，剩余2ca000 - 1b7000 = 113000
+    -- 程序区缩小到1b7000，剩余2c2000 - 1b7000 = 10b000
     if chip_target == "ec718p" or chip_target == "ec716e" or chip_target == "ec718e" then
         add_defines("AP_FLASH_LOAD_SIZE=0x1b7000",{public = true})
         add_defines("AP_PKGIMG_LIMIT_SIZE=0x1b7000",{public = true})
