e9292891c566d2bc5eed54ea78270bc6ee482d44
alienwalker <alienwalker@sina.com>
2024-08-23 14:12:00 +0800

    update:iconv demo完善

--- project/example_iconv/src/iconv_main.c
+++ project/example_iconv/src/iconv_main.c
@@ -31,34 +31,49 @@
 luat_rtos_task_handle task_handle;
 
 
+#define OUT_BUF_LEN 256
 static void demo_init_iconv()
 {
-	LUAT_DEBUG_PRINT("==================iconv is running==================");
-
-	char input[] = "LuatOS is coming", output[255];
-	memset(output, 0, sizeof(output));
-
-	char* inPtr = input;
-	char* outPtr = output;
-	size_t inlen = strlen(input);
-	size_t outlen = sizeof(output);
-
-	LUAT_DEBUG_PRINT("The convert inlen is %d", inlen);
-	LUAT_DEBUG_PRINT("The convert outlen is %d", outlen);
-
-	luat_iconv_t cd = luat_iconv_open("utf8", "ucs2");
-	int len = luat_iconv_convert(cd, &inPtr, &inlen, &outPtr, &outlen);
-	LUAT_DEBUG_PRINT("The convert num is %d", len);
-	if (len == 0)
+	LUAT_DEBUG_PRINT("演示GBK转UTF8");
+	//支付宝收款200元，感谢使用的GBK编码数据，为了防止源码的编码方式带来的影响，直接使用数组
+	char org_string[] = {0xD6,0xA7,0xB8,0xB6,0xB1,0xA6,0xCA,0xD5,0xBF,0xEE,0x32,0x30,0x30,0xD4,0xAA,0xA3,0xAC,0xB8,0xD0,0xD0,0xBB,0xCA,0xB9,0xD3,0xC3,0x00};
+	char *input = org_string; //不能直接传数组，必须传指针
+	char buf1[OUT_BUF_LEN] = {0};
+	char buf2[OUT_BUF_LEN] = {0};
+	size_t input_len = sizeof(org_string);
+	size_t out_buffer_rest_len = OUT_BUF_LEN;
+	luat_iconv_t cd = luat_iconv_open("ucs2", "gb2312");
+	if (cd == (void *)-1)
 	{
-		LUAT_DEBUG_PRINT("The convert before: %s", input);
-		LUAT_DEBUG_PRINT("The convert  after: %s", outPtr);
+		LUAT_DEBUG_PRINT("iconv not support");
+		return;
 	}
+	char *ucs2_out = buf1;	//不能直接传数组，必须传指针
+	char *utf8_out = buf2;  //不能直接传数组，必须传指针
+	int result = luat_iconv_convert(cd, &input, &input_len, &ucs2_out, &out_buffer_rest_len);
 	luat_iconv_close(cd);
+	if (result)
+	{
+		LUAT_DEBUG_PRINT("The convert failed %d", result);
+		return;
+	}
+	input_len = OUT_BUF_LEN - out_buffer_rest_len;
+	LUAT_DEBUG_PRINT("ucs2 len%d", input_len);
+	cd = luat_iconv_open("utf8", "ucs2");
+	out_buffer_rest_len = OUT_BUF_LEN;
+	result = luat_iconv_convert(cd, &ucs2_out, &input_len, &utf8_out, &out_buffer_rest_len);
+	luat_iconv_close(cd);
+	if (result)
+	{
+		LUAT_DEBUG_PRINT("The convert failed %d", result);
+		return;
+	}
+	LUAT_DEBUG_PRINT("转换结果, %.*s", OUT_BUF_LEN - out_buffer_rest_len, utf8_out);
 }
 
 static void task(void *param)
 {
+	luat_debug_set_fault_mode(LUAT_DEBUG_FAULT_HANG);
 	while(1)
 	{
 		demo_init_iconv();
@@ -70,8 +85,7 @@ static void task(void *param)
 
 static void task_demoE_init(void)
 {
-	luat_rtos_task_create(&task_handle, 2*1024, 50, "task", task, NULL, 0);
+	luat_rtos_task_create(&task_handle, 4*1024, 50, "task", task, NULL, 0);
 }
 
-//启动task_demoE_init，启动位置任务1级
 INIT_TASK_EXPORT(task_demoE_init, "1");
