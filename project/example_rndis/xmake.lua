0172f18fc315cb8ed5dbaa16d933cf50ac2cf27c
alienwalker <alienwalker@sina.com>
2024-08-30 10:33:23 +0800

    update:rndis demo改成默认NAT模式

--- project/example_rndis/xmake.lua
+++ project/example_rndis/xmake.lua
@@ -10,8 +10,8 @@ target(project_name,function()
     set_targetdir("$(buildir)/".. project_name .. "/")
     description_csdk()
     set_warnings("error")
-
-    add_defines("LUAT_USE_RNDIS",{public = true})
+	-- add_defines("LUAT_USE_RNDIS",{public = true}) -- 一般公网卡可以激活多个APN，不需要LUAT_USE_RNDIS_NAT_MODE，但是很多时候用户不确定，所以还是默认打开
+	add_defines("LUAT_USE_RNDIS","LUAT_USE_RNDIS_NAT_MODE",{public = true})
     --加入代码和头文件
     add_includedirs("./inc",{public = true})
     add_files("./src/*.c",{public = true})
