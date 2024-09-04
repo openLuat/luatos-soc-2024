2ced5c67107ef1e727196a6a06aff14e5261c16b
Wendal Chen <wendal1985@gmail.com>
2024-09-04 15:03:14 +0800

    update: conf_bsp按型号拆分,但没有启用, 会报错

---
+++ project/luatos/inc/luat_conf_bsp_air201.h
@@ -0,0 +1,56 @@
+
+// Air201 基本上就是 Air780EPS的基础上开启UI库, 但不开LVGL
+
+#define LUAT_USE_UART 1
+#define LUAT_USE_GPIO 1
+#define LUAT_USE_I2C  1
+#define LUAT_USE_SPI  1
+#define LUAT_USE_ADC  1
+#define LUAT_USE_PWM  1
+#define LUAT_USE_WDT  1
+#define LUAT_USE_PM  1
+#define LUAT_USE_MCU  1
+#define LUAT_USE_RTC 1
+#define LUAT_USE_OTP 1
+#define LUAT_USE_WLAN 1
+#define LUAT_USE_HMETA 1
+
+#define LUAT_USE_IOTAUTH 1
+#define LUAT_USE_MQTT 1
+#define LUAT_USE_WEBSOCKET 1
+#define LUAT_USE_SOFT_UART 1
+
+#define LUAT_USE_CRYPTO  1
+#define LUAT_USE_CJSON  1
+#define LUAT_USE_ZBUFF  1
+#define LUAT_USE_PACK  1
+#define LUAT_USE_LIBGNSS  1
+#define LUAT_USE_FS  1
+#define LUAT_USE_SENSOR  1
+
+#define LUAT_USE_FSKV 1
+#define LUAT_USE_I2CTOOLS 1
+#define LUAT_USE_MINIZ 1
+#define LUAT_USE_FTP 1
+#define LUAT_USE_BIT64 1
+
+#define LUAT_USE_ICONV 1
+
+#define LUAT_USE_MEDIA    1
+#define LUAT_USE_RECORD     1
+#define LUAT_USE_TTS    1
+#define LUAT_USE_TTS_ONCHIP    1
+
+#define LUAT_SCRIPT_SIZE 512
+#define LUAT_SCRIPT_OTA_SIZE 360
+
+// 开启UI库
+#define LUAT_USE_UI    1
+#define LUAT_USE_LCD
+#define LUAT_USE_TJPGD
+#define LUAT_USE_EINK
+#define LUAT_USE_U8G2
+#define LUAT_USE_FONTS
+#define USE_U8G2_OPPOSANSM_ENGLISH 1
+#define USE_U8G2_OPPOSANSM12_CHINESE
+#define USE_U8G2_OPPOSANSM16_CHINESE
