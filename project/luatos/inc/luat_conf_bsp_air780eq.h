5fff9e3022c25af48a35b6558bf08d5395a25265
Wendal Chen <wendal1985@gmail.com>
2024-09-14 19:00:45 +0800

    update: 按最新的定义,780eq调整库的配置

--- project/luatos/inc/luat_conf_bsp_air780eq.h
+++ project/luatos/inc/luat_conf_bsp_air780eq.h
@@ -5,16 +5,17 @@
 
 #define LUAT_USE_UART 1
 #define LUAT_USE_GPIO 1
-#define LUAT_USE_I2C  1
-#define LUAT_USE_SPI  1
-#define LUAT_USE_ADC  1
-#define LUAT_USE_PWM  1
+// #define LUAT_USE_I2C  1
+// #define LUAT_USE_SPI  1
+// #define LUAT_USE_ADC  1
+// #define LUAT_USE_PWM  1
 #define LUAT_USE_WDT  1
 #define LUAT_USE_PM  1
 #define LUAT_USE_MCU  1
 #define LUAT_USE_RTC 1
-#define LUAT_USE_OTP 1
+// #define LUAT_USE_OTP 1
 #define LUAT_USE_WLAN 1
+#define LUAT_USE_SMS 1
 #define LUAT_USE_HMETA 1
 
 #define LUAT_USE_IOTAUTH 1
@@ -26,9 +27,9 @@
 #define LUAT_USE_CJSON  1
 #define LUAT_USE_ZBUFF  1
 #define LUAT_USE_PACK  1
-#define LUAT_USE_LIBGNSS  1
+// #define LUAT_USE_LIBGNSS  1
 #define LUAT_USE_FS  1
-#define LUAT_USE_SENSOR  1
+// #define LUAT_USE_SENSOR  1
 
 #define LUAT_USE_FSKV 1
 // #define LUAT_USE_I2CTOOLS 1
@@ -43,5 +44,7 @@
 // #define LUAT_USE_TTS    1
 // #define LUAT_USE_TTS_ONCHIP    1
 
-#define LUAT_SCRIPT_SIZE 512
-#define LUAT_SCRIPT_OTA_SIZE 360
+#define LUAT_SCRIPT_SIZE 256
+#define LUAT_SCRIPT_OTA_SIZE 192
+
+#define LUAT_HEAP_SIZE 200
