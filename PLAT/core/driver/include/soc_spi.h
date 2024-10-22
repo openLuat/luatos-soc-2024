21cda786d53ec1e24de74c952e9d804831ebcce8
Wendal Chen <wendal1985@gmail.com>
2024-10-22 12:01:06 +0800

    add: csdk update a7e1898a

--- PLAT/core/driver/include/soc_spi.h
+++ PLAT/core/driver/include/soc_spi.h
@@ -15,4 +15,8 @@ void SPI_SetDMAEnable(uint8_t SpiID, uint8_t IsEnable);
 void SPI_SetNewConfig(uint8_t SpiID, uint32_t Speed, uint8_t NewMode);
 void SPI_SetDMATrigger(uint8_t SpiID, uint32_t UpValue, uint32_t DownValue);
 uint32_t SPI_GetSpeed(uint8_t SpiID);
+
+void SPI_SlaveInit(uint8_t SpiID, uint8_t DataBit, uint8_t Mode, uint32_t Speed, CBFuncEx_t CB, void *pUserData);
+int SPI_SlaveTransferStart(uint8_t SpiID, uint8_t* TxBuffer, uint8_t* RxBuffer, uint16_t BufferLen);
+uint32_t SPI_SlaveTransferStopAndGetRxLen(uint8_t SpiID);
 #endif
