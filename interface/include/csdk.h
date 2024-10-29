8c239c455d712dbac9ad65fbc0bc0b7b5c68d440
alienwalker <alienwalker@sina.com>
2024-10-29 09:46:52 +0800

    add:常用csdk文件

---
+++ interface/include/csdk.h
@@ -0,0 +1,27 @@
+#ifndef __CSDK_H__
+#define __CSDK_H__
+#include "luat_base.h"
+#include "common_api.h"
+
+#include "luat_adc.h"
+#include "luat_audio.h"
+#include "luat_audio_codec.h"
+#include "luat_audio_play.h"
+#include "luat_gpio.h"
+#include "luat_fota.h"
+#include "luat_debug.h"
+#include "luat_fs.h"
+#include "luat_mem.h"
+#include "luat_i2c.h"
+#include "luat_pm.h"
+#include "luat_mcu.h"
+#include "luat_i2s.h"
+#include "luat_rtos.h"
+#include "luat_rtc.h"
+#include "luat_uart.h"
+#include "luat_spi.h"
+
+#include "luat_mobile.h"
+#include "luat_network_adapter.h"
+#include "net_lwip.h"
+#endif
